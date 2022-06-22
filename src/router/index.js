7f3704156e300d83941124569266416fe6592cee
hojin <sungsill150@gmail.com>
2022-06-22 22:21:58 +0900

    fff

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/list',
+    name: 'list',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "list" */ '../views/InterListView.vue'),
+  },
 ];
 
 const router = createRouter({
